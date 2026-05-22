77d5492d7addd03716d955f757d51af7bb8a8291
Camarederic <camarederic@gmail.com>
2026-05-22 22:53:27 +0300

    added code for the BMI Calculator JS

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-'use strict'
+"use strict";
 
+const btnEl = document.getElementById("btn");
+const bmiResult = document.getElementById("bmi-result");
+const weightCondition = document.getElementById("weight-condition");
+
+function calculateBMI() {
+  const heightValue = document.getElementById("height").value / 100;
+  const weightValue = document.getElementById("weight").value;
+
+  const bmiValue = weightValue / (heightValue * heightValue);
+
+  bmiResult.value = bmiValue;
+  if (bmiValue < 18.5) {
+    weightCondition.innerText = "Under weight";
+  } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+    weightCondition.innerText = "Normal weight";
+  } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+    weightCondition.innerText = "Overweight";
+  } else if (bmiValue > 30) {
+    weightCondition.innerText = "Obesity";
+  }
+}
+
+btnEl.addEventListener("click", calculateBMI);
